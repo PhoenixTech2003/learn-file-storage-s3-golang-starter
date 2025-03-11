5ecf00d4cb958101801731f843d5d5e2fbb5c671
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com>
2025-03-11 16:13:34 +0200

    Implement in memory file storage

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/google/uuid"
 )
 
@@ -28,10 +32,55 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	mediaType := header.Header.Get("Content-Type")
+	imageData, err := io.ReadAll(file)
+	if err != nil {
+		log.Printf("An error occured while reading image data %s", err)
+		respondWithError(w, http.StatusInternalServerError, "an error occured while parsing file", err)
+		return
+	}
+	videoData, err := cfg.db.GetVideo(videoID)
+
+	if videoData.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "an error occured while requesting this resource", err)
+		return
+	}
+
+	videoThumbnail := thumbnail{
+		mediaType: mediaType,
+		data:      imageData,
+	}
+
+	videoThumbnails[videoID] = videoThumbnail
+	thumbnailUrl := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+	updateVideoParams := database.Video{
+		ID:           videoID,
+		UpdatedAt:    time.Now(),
+		ThumbnailURL: &thumbnailUrl,
+		VideoURL:     videoData.VideoURL,
+		CreateVideoParams: database.CreateVideoParams{
+			Title:       videoData.Title,
+			Description: videoData.Description,
+			UserID:      userID,
+		},
+	}
+	err = cfg.db.UpdateVideo(updateVideoParams)
+	if err != nil {
+		log.Printf("An error occured while updating the thubnail %s", err)
+		respondWithError(w, http.StatusInternalServerError, "an error occured while updating the thubmnail url", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, updateVideoParams)
 }
